b2ccbccb77ce650e18f11805d0b290b887e1e1b1
Mikhail <94560571+mikhailmtion@users.noreply.github.com>
2024-05-02 21:49:02 +0300

    update layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,33 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 export default function RootLayout() {
+//   const insets = useSafeAreaInsets();
+
   return (
-    <>
-      <Stack />
-    </>
+    <SafeAreaProvider>
+      <StatusBar style="light" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            // paddingTop: insets.top,
+          },
+        }}
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen
+          name="restore"
+          options={{
+            title: "Восстановление пароля",
+            presentation: "modal",
+          }}
+        />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
